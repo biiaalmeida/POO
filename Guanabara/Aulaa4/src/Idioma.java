92b8f16b658b816afa6af5025fd7b808719c5da1
biiaalmeida <abeatrizpb@gmail.com>
2025-04-28 22:20:29 -0300

    Initial commit

---
+++ Guanabara/Aulaa4/src/Idioma.java
@@ -0,0 +1,10 @@
+import java.util.Locale;
+
+public class Idioma {
+    public static void main(String[] args){
+        Locale locale = Locale.getDefault();
+
+        System.out.println("O idioma é");
+        System.out.println(locale);
+    }
+}
---
+++ Guanabara/Aulaa4/src/Resolucao.java
@@ -0,0 +1,12 @@
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+public class Resolucao {
+    public static void main(String[] args){
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        Dimension screenSize = toolkit.getScreenSize();
+        
+        System.out.println("A resolução é:");
+        System.out.println(screenSize.width + " x " + screenSize.height);
+    }
+}
