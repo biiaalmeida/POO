287e0bbccf70bd76ba95c6639bd4f5aad68f9259
biiaalmeida <abeatrizpb@gmail.com>
2025-04-29 08:55:50 -0300

    Add OperadoresAritmeticos

---
+++ Guanabara/OperadoresAritmeticos.java
@@ -0,0 +1,24 @@
+package POO.Guanabara;
+
+public class OperadoresAritmeticos {
+    
+    public static void main(String[] args) {
+        /*int n1 = 3;
+        int n2 = 5;
+        float m = (n1 + n2)/2;
+        System.out.println("A média é: " + m);*/
+
+        /*int numero = 5;
+        numero = 5 + numero++; 
+        //-> pos incremento =10
+        numero = 5 + ++numero; 
+        //-> pré incremento =11
+        System.out.println(numero);*/
+
+        /*int x = 4;
+        x += 2; 
+        //x recebe x + 2
+        System.out.print(x);*/
+
+    }
+}
