5bacadf171a84dae986836cfe87ff93fc1a8918a
biiaalmeida <abeatrizpb@gmail.com>
2025-05-07 16:44:44 -0300

    Include classroom exercises

---
+++ UnidadeI/classes/eventos/DemoEventos.java
@@ -0,0 +1,8 @@
+package POO.UnidadeI.classes.eventos;
+
+public class DemoEventos {
+    public static void main(String[] args) {
+        EventoAcademico ev = new EventoAcademico("ECOP", "PDF", 1);
+        ev.mostraEvento();
+    }
+}
