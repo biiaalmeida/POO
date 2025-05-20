a196e7059c1385fcda5bed2f447ce2c82b20fc0e
biiaalmeida <abeatrizpb@gmail.com>
2025-05-20 12:03:57 -0300

    Commented question 5 prompt in the code

--- UnidadeI/IntroJava/Lista/Questao5.java
+++ UnidadeI/IntroJava/Lista/Questao5.java
@@ -1,3 +1,8 @@
+/*5. Escreva um programa que receba um número de 3 dígitos no formato CDU (Centena,
+Dezena e Unidade) e, utilizando operadores aritméticos, armazene cada dígito em uma
+variável de tipo inteiro. Por fim, reescreva o número no formato UCD em outra variável
+de tipo inteiro e o imprima. Exemplo: 123 deve ser reescrito como 312. */
+
 package POO.UnidadeI.IntroJava.Lista;
 import java.util.Scanner;
 
