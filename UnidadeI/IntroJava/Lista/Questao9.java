48eaa8924cd321e258ef4d724137530c74892b15
biiaalmeida <abeatrizpb@gmail.com>
2025-05-21 14:52:57 -0300

    Fix and improve logic

--- UnidadeI/IntroJava/Lista/Questao9.java
+++ UnidadeI/IntroJava/Lista/Questao9.java
@@ -8,26 +8,45 @@ import java.util.Scanner;
 public class Questao9 {
     public static void main(String[] args) {
         int num, naipe;
+        String nomeCarta;
         Scanner leitor = new Scanner(System.in);
 
         System.out.print("Valor da carta: ");
         num = leitor.nextInt();
 
+        System.out.print("1 = ouros, 2= paus, 3 = copas e 4 = espada");
         System.out.print("Naipe: ");
         naipe = leitor.nextInt();
         
+        switch (num) {
+            case 1:
+                nomeCarta = "ás";
+                break;
+            case 11: 
+                nomeCarta = "valete";
+                break;
+            case 12:
+                nomeCarta = "dama";
+                break;
+            case 13: 
+                nomeCarta = "rei";
+                break;
+            default:
+                nomeCarta = String.valueOf(num); // Mantém o número como texto
+                break;
+        }
         switch (naipe) {
             case 1:
-                System.out.printf("Sua carta é %d de ouros", num);
+                System.out.printf("Sua carta é %s de ouros", nomeCarta);
                 break;
             case 2: 
-                System.out.printf("Sua carta é %d de paus", num);
+                System.out.printf("Sua carta é %s de paus", nomeCarta);
                 break;
             case 3: 
-                System.out.printf("Sua carta é %d de copas", num);
+                System.out.printf("Sua carta é %s de copas", nomeCarta);
                 break;
             case 4: 
-                System.out.printf("Sua carta é %d de espadas", num);
+                System.out.printf("Sua carta é %s de espadas", nomeCarta);
                 break;
             default:
                 System.out.print("naipe inválido");
